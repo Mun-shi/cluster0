030ae5fce291dd64a6affbf3cb957a0cdb205078
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2022-02-11 12:38:16 +0530

    update

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -35,10 +35,20 @@ router.post(
    [
     check('status', 'status is required')
     .not()
+    .isEmpty(),
+    check('skills', 'skills is required')
+    .not()
     .isEmpty()
    ]
 ],
-(req, res) =>{}
+ async (req, res) =>{
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }
+
+    
+ }
 );
 
 module.exports = router;  
