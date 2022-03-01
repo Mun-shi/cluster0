6a4b3f3cef57a33e0089f15ede7eb57946272b14
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2022-03-01 20:44:09 +0530

    update

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -77,8 +77,8 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
- // @route   GET api/posts
- // @desk    Get all posts
+ // @route   delete api/post/:id
+ // @desk    delete  post by id
  // @access  private
  router.delete('/:id', auth, async (req, res) => {
     try {
@@ -103,7 +103,29 @@ router.get('/', auth, async (req, res) => {
     }
      res.status(500).send('server error');
     }
-}); 
+});
 
+ // @route   delete api/posts/like/:id
+ // @desk    like a post 
+ // @access  private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      
+      // check if the post has a already been liked
+      if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+        return res.status(400).json({ msg: 'post already liked'});
+      }
+
+      post .likes.unshift({ user: req.user.id });
+
+      await post.save();
+
+      res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+});
 
  module.exports = router;
