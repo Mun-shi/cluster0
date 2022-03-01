9d62fdd2a54ef25cb92025d6808b2517321e5f8b
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2022-03-01 12:52:46 +0530

    update post.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,47 @@
 const express = require('express');
- const router = express.Router(); 
+ const router = express.Router();
+ const { check, validationResult } = require('express-validator/check');
+ const auth = require('../../routes/api/middleware/auth');
 
- // @route   GET api/posts
- // @desk    test route
- // @access  public
- router.get('/',(req, res) => res.send('posts route'));
+const Post = require('../../routes/api/models/Post');
+const Profile = require('../../routes/api/models/Profile');
+const User = require('../../routes/api/models/User');
+
+ // @route   POST api/posts
+ // @desk    create a post
+ // @access  private
+ router.post('/', [ auth, [
+    check('text', 'Text is required')
+      .not()
+      .isEmpty()
+ ]
+ ] , 
+  async  (req, res) => {
+      const errors = validationResult(req);
+      if(!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array()});
+      }
+      try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post ({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+
+         const post = await newPost.save();
+
+         res.json(post);
+          
+      } catch (err) {
+         console.error(err.message);
+         res.status(500).send('server error'); 
+      }
+    
+      
+  }
+ );
 
  module.exports = router;
